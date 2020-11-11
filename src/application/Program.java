1ac3a59d82b027d3849d3a109e462549e1f76ba3
Ricardo Souza <rcss2311@gmail.com>
2020-11-11 18:38:05 -0300

    Equals e HasCode

---
+++ src/application/Program.java
@@ -0,0 +1,28 @@
+package application;
+
+import entities.Cliente;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Cliente c1 = new Cliente("Maria","maria@gmail.com");
+		Cliente c2 = new Cliente("Maria","maria@gmail.com");
+		
+		String s1 = "Teste";
+		String s2 = "Teste";
+		String s3 = new String("Teste");
+		String s4 = new String("Teste");
+		
+		
+		
+		System.out.println(c1.hashCode());
+		System.out.println(c2.hashCode());
+		System.out.println(c1.equals(c2));
+		System.out.println(c1 ==c2);
+		System.out.println(s1 == s2);
+		System.out.println(s3 == s4);
+	}
+	
+	
+
+}
